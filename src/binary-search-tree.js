102213eea4957f11d899abb22bd660933f41c83b
binary-apple <112655570+binary-apple@users.noreply.github.com>
2022-10-17 14:01:23 +0200

    feat: binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,134 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor(r = null) {
+    this.Root = r;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.Root === null) {
+      this.Root = new Node(data);
+      return;
+    } 
+    let curr = this.Root;
+    while (1) {
+      if (data < curr.data) {
+        if (curr.left === null) { curr.left = new Node(data); return; }
+        else { curr = curr.left; continue; }
+      }
+      if (data > curr.data) {
+        if (curr.right === null) { curr.right = new Node(data); return; }
+        else { curr = curr.right; continue; }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.Root === null) {
+      return false;
+    }
+    let curr = this.Root;
+    while (1) {
+      if (data == curr.data) { return true; }
+      if (data < curr.data) {
+        if (curr.left === null) { return false; }
+        else { curr = curr.left; continue; }
+      }
+      if (data > curr.data) {
+        if (curr.right === null) { return false; }
+        else { curr = curr.right; continue; }
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (this.Root === null) {
+      return null;
+    }
+    let curr = this.Root;
+    while (1) {
+      if (data == curr.data) { return curr; }
+      if (data < curr.data) {
+        if (curr.left === null) { return null; }
+        else { curr = curr.left; continue; }
+      }
+      if (data > curr.data) {
+        if (curr.right === null) { return null; }
+        else { curr = curr.right; continue; }
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (this.Root === null) {
+      return null;
+    }
+    let curr = this.Root;
+    let parent = new Node;
+    while (1) {
+      if (data == curr.data) { break; }
+      if (data < curr.data) {
+        if (curr.left === null) { return null; }
+        else { parent = curr; curr = curr.left; continue; }
+      }
+      if (data > curr.data) {
+        if (curr.right === null) { return null; }
+        else { parent = curr; curr = curr.right; continue; }
+      }
+    }
+    //no children
+    if (!(curr.left) && !(curr.right)) { 
+      if (parent.left == curr) { parent.left = null; return; }
+      if (parent.right == curr) { parent.right = null; return; }
+    }
+    //one child
+    if (curr.left && !(curr.right)) {
+      if (parent.left == curr) { parent.left = curr.left; return; }
+      if (parent.right == curr) { parent.right = curr.left; return; }
+    }
+    if (!(curr.left) && curr.right) {
+      if (parent.left == curr) { parent.left = curr.right; return; }
+      if (parent.right == curr) { parent.right = curr.right; return; }
+    }
+    //two children
+    if (curr.left && curr.right) {
+     let tmp = new BinarySearchTree(curr.right);
+     curr.data = tmp.min();
+     tmp.remove(curr.data);
+    }
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.Root === null) {
+      return null;
+    }
+    let curr = this.Root;
+    while (curr.left) {
+      curr = curr.left;
+    }
+    return curr.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.Root === null) {
+      return null;
+    }
+    let curr = this.Root;
+    while (curr.right) {
+      curr = curr.right;
+    }
+    return curr.data;
   }
 }
 
